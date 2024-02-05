80efc2d2b7e6a70e707032b6c262c9da2f1bce1a
elusojitimileyin <elusojitimileyin@gmail.com>
2024-02-05 13:49:09 +0100

    timi AirConditioner

---
+++ src/AirConditioner.java
@@ -0,0 +1,39 @@
+public class AirConditioner {
+
+    private boolean isOn;
+    private boolean isOff;
+    private int temperature = 16 ;
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public void setOn(boolean on) {
+        isOn = on;
+    }
+
+    public boolean isOff() {
+        return isOff;
+    }
+
+    public void setOff(boolean off) {
+        isOff = off;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void increaseTemperature() {
+
+        if (isOn && temperature >= 16 && temperature < 30) temperature++; {
+
+        }
+    }
+
+    public void decreaseTemperature() {
+        if (isOn && temperature > 16 && temperature <= 30) temperature--; {
+
+        }
+    }
+}
---
+++ test/AirConditionerTest.java
@@ -0,0 +1,83 @@
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AirConditionerTest {
+
+    private AirConditioner myAirconditioner;
+
+    @BeforeEach
+    void intializeAC() {
+        myAirconditioner = new AirConditioner();
+    }
+
+    @Test
+    void testAcIsOn() {
+        myAirconditioner.setOff(true);
+        assertTrue(myAirconditioner.isOff());
+
+        myAirconditioner.setOn(true);
+        assertTrue(myAirconditioner.isOn());
+
+    }
+
+    @Test
+    void testACIsOff() {
+
+        myAirconditioner.setOff(true);
+        assertTrue(myAirconditioner.isOff());
+
+        myAirconditioner.setOn(true);
+        assertTrue(myAirconditioner.isOn());
+
+        myAirconditioner.setOff(true);
+        assertTrue(myAirconditioner.isOff());
+    }
+
+    @Test
+    void testACTemperatureIsIncreasing() {
+        myAirconditioner.setOn(true);
+        assertTrue(myAirconditioner.isOn());
+
+        myAirconditioner.increaseTemperature();
+        assertEquals(16, myAirconditioner.getTemperature());
+        myAirconditioner.increaseTemperature();
+        myAirconditioner.increaseTemperature();
+        assertEquals(17, myAirconditioner.getTemperature());
+
+    }
+
+    @Test
+    void testACTemperatureIsDecreasing() {
+        myAirconditioner.setOn(true);
+        assertTrue(myAirconditioner.isOn());
+        myAirconditioner.increaseTemperature();
+        myAirconditioner.increaseTemperature();
+        assertEquals(18, myAirconditioner.getTemperature());
+
+        myAirconditioner.decreaseTemperature();
+        assertEquals(17, myAirconditioner.getTemperature());
+    }
+    @Test
+    public void increaseAcTemperatureAt30_temperatureIs30Test() {
+        myAirconditioner.setOn(true);
+        assertTrue(myAirconditioner.isOn());
+
+        for (int count = 0; count < 14; count++) myAirconditioner.increaseTemperature();
+        assertEquals(30, myAirconditioner.getTemperature());
+
+        myAirconditioner.increaseTemperature();
+        assertEquals(30, myAirconditioner.getTemperature());
+    }
+
+    @Test
+    public void decreaseAcTemperatureAt16_temperatureIs16Test() {
+        myAirconditioner.setOn(true);
+        assertTrue(myAirconditioner.isOn());
+
+        myAirconditioner.decreaseTemperature();
+        assertEquals(16, myAirconditioner.getTemperature());
+    }
+}
